f0f293909285cd036e04a30aa4f6c6cc859172fe
root <root@popsaserver.ddns.net>
2025-12-12 19:03:54 +0000

    v1.2

--- public/script.mjs
+++ public/script.mjs
@@ -22,43 +22,100 @@ typing_message.style.display = "none";
 send_button.addEventListener("click", function() {
     if (message_input.value.trim() !== "" && num_queued_messages < 5) {
         user_message = message_input.value;
-        num_queued_messages++;
-        conversation_history = sessionStorage.getItem('conversation_history');
-
         message_input.value = "";
         let new_user_message = document.createElement("p");
         new_user_message.classList.add("user_message_object");
         new_user_message.textContent = user_message;
         message_area.prepend(new_user_message);
 
-        message_area.prepend(typing_message);
-        typing_message.style.display = "block";
+        let new_bot_message = document.createElement("p");
+        new_bot_message.classList.add("bot_message_object");
 
-        $.ajax({
-          url: '/request',
-          type: "POST",
-          contentType: "text/plain",
-          data: conversation_history + delimiter + user_message,
-          dataType: 'text',
-          success: function(response) {
-            let new_bot_message = document.createElement("p");
-            new_bot_message.classList.add("bot_message_object");
-            new_bot_message.textContent = response;
+        if (user_message.trim() === "/aboutme") {
+            new_bot_message.textContent = "[placeholder for future content] the name's potluri. saaket potluri";
             message_area.prepend(new_bot_message);
+        }
 
-            if (--num_queued_messages > 0) {
-                message_area.prepend(typing_message);
-            }
+        else if (user_message.trim() === "/commands") {
+            new_bot_message.textContent = "'/aboutme', '/commands', '/github', '/highlights', '/linkedin', '/projects', '/reset', '/resume'";
+            message_area.prepend(new_bot_message);
+        }
 
-            else {
-                typing_message.style.display = "none";
-            }
-            sessionStorage.setItem('conversation_history', conversation_history + "<|start|>user<|message|>" + user_message + '<|end|>\n' + "<|start|>assistant<|message|>" + response + '<|end|>\n');
-          }, 
-          error: function(xhr, status, error) {
-            console.error("AJAX Error: ", status, error);
-          }
-        });
+        else if (user_message.trim() === "/explore") {
+            new_bot_message.textContent = "saaket here. welcome to my website! i wanted to make this site as unique as possible, so instead of having a bunch of webpages, all that information can be found by putting commands into the message bar below. if you just wanna chat with saaketgpt, type in any message and it'll respond as soon as possible. otherwise, here are a few commands to get you started:\n'/aboutme', '/highlights', '/resume' (use '/commands' for the full list)";
+            message_area.prepend(new_bot_message);
+        }
+
+        else if (user_message.trim() === "/github") {
+            window.open("https://github.com/Popsa676", "_blank");
+            new_bot_message.textContent = "[opened github in new tab]";
+            message_area.prepend(new_bot_message);
+        }
+
+        else if (user_message.trim() === "/highlights") {
+            new_bot_message.textContent = "[placeholder for future content] 50 pounds lost. 50-mile bike ride. 5:50 mile time. 15 pullups. 1500 PSAT";
+            message_area.prepend(new_bot_message);
+        }
+
+        else if (user_message.trim() === "/linkedin") {
+            window.open("https://www.linkedin.com/in/saaket-potluri-668488199/", "_blank");
+            new_bot_message.textContent = "[opened linkedin in new tab]";
+            message_area.prepend(new_bot_message);
+        }
+
+        else if (user_message.trim() === "/projects") {
+            new_bot_message.textContent = "[placeholder for future content] use '/resume' for brief project descriptions";
+            message_area.prepend(new_bot_message);
+        }
+
+        else if (user_message.trim() === "/reset") {
+            sessionStorage.setItem('conversation_history', '');
+            message_area.replaceChildren();
+            new_bot_message.textContent = "hey";
+            message_area.prepend(new_bot_message);
+            let new_bot_message2 = document.createElement("p");
+            new_bot_message2.classList.add("bot_message_object");
+            new_bot_message2.textContent = "whats up";
+            message_area.prepend(new_bot_message2);
+        }
+
+        else if (user_message.trim() === "/resume") {
+            window.open("https://drive.google.com/file/d/1qi_lVRh8b19T2L1YzaIiClU_nf_PmD_S/view?usp=sharing", "_blank");
+            new_bot_message.textContent = "[opened resume in new tab]";
+            message_area.prepend(new_bot_message);
+        }
+
+        else {
+            num_queued_messages++;
+            conversation_history = sessionStorage.getItem('conversation_history');
+
+            message_area.prepend(typing_message);
+            typing_message.style.display = "block";
+
+            $.ajax({
+              url: '/request',
+              type: "POST",
+              contentType: "text/plain",
+              data: conversation_history + delimiter + user_message,
+              dataType: 'text',
+              success: function(response) {
+                new_bot_message.textContent = response;
+                message_area.prepend(new_bot_message);
+
+                if (--num_queued_messages > 0) {
+                    message_area.prepend(typing_message);
+                }
+
+                else {
+                    typing_message.style.display = "none";
+                }
+                sessionStorage.setItem('conversation_history', conversation_history + "<|start|>user<|message|>" + user_message + '<|end|>\n' + "<|start|>assistant<|message|>" + response + '<|end|>\n');
+              }, 
+              error: function(xhr, status, error) {
+                console.error("AJAX Error: ", status, error);
+              }
+            });
+        }
     }
 
     else {
