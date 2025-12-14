fd4cac0135e25483056e569d6b04ad9b7ed4283e
root <root@popsaserver.ddns.net>
2025-12-14 20:45:01 +0000

    v1.5

--- public/script.mjs
+++ public/script.mjs
@@ -31,39 +31,39 @@ send_button.addEventListener("click", function() {
         let new_bot_message = document.createElement("p");
         new_bot_message.classList.add("bot_message_object");
 
-        if (user_message.trim() === "aboutme") {
+        if (user_message.trim().toLowerCase() === "about") {
             new_bot_message.textContent = "[saaket] hi! my name's saaket potluri and i'm a second-year student at the university of maryland majoring in computer science and minoring in math. my programming journey began with block coding lego robots almost 10 years ago, and over that period i've become versed in python, java, c, and javascript with experience in web development, app development, and systems engineering. my most recent passion is ai, which led me to making saaketgpt and will be the focus of my future projects too. when i'm not studying for exams or working on projects, i spend my time lifting at the gym or playing matches on the badminton courts. my other hobbies include biking, reading comics, and playing video games.";
             message_area.prepend(new_bot_message);
         }
 
-        else if (user_message.trim() === "bestmoment") {
-            new_bot_message.textContent = "[saaket] i would say my best moment was when i accomplished losing 50 pounds in one year. i'd struggled with my weight my whole life even with a pretty active lifestyle and trying different diets. it only got worse when i stopped playing sports because of covid, causing my weight to increase to 200 pounds by the beginning of my first year at university. after coming to umd, i started playing badminton for a few hours almost everyday combined with lifting and intermittent fasting. over the following school year, i lost 45 pounds while also increasing my muscle strength significantly. after summer break, i came back to school and lost 5 more pounds in the first few weeks of the semester, bringing me to a total of 50 pounds lost.";
+        else if (user_message.trim().toLowerCase() === "bestmoment") {
+            new_bot_message.textContent = "[saaket] the best moment of my life was when i accomplished losing 50 pounds in one year. i'd struggled with my weight my whole life even with a pretty active lifestyle and trying different diets. it only got worse when i stopped playing sports because of covid, causing my weight to increase to 200 pounds by the beginning of my first year at university. after coming to umd, i started playing badminton for a few hours almost everyday combined with lifting and intermittent fasting. over the following school year, i lost 45 pounds while also increasing my muscle strength significantly. after summer break, i came back to school and lost 5 more pounds in the first few weeks of the semester, bringing me to a total of 50 pounds lost.";
             message_area.prepend(new_bot_message);
         }
 
-        else if (user_message.trim() === "commands") {
-            new_bot_message.textContent = "'aboutme', 'bestmoment', 'commands', 'github', 'linkedin', 'reset', 'resume'";
+        else if (user_message.trim().toLowerCase() === "commands") {
+            new_bot_message.textContent = "'about', 'bestmoment', 'commands', 'github', 'linkedin', 'reset', 'resume', 'saaketgpt'";
             message_area.prepend(new_bot_message);
         }
 
-        else if (user_message.trim() === "explore") {
+        else if (user_message.trim().toLowerCase() === "explore") {
             new_bot_message.textContent = "[saaket] welcome to my website! i wanted to make this site as unique as possible, so instead of having a bunch of webpages, all that information can be found by putting commands into the message bar below. if you just wanna chat with saaketgpt, type in any message and it'll respond as soon as possible. otherwise, here are a few commands to get you started:\n'aboutme', 'bestmoment', 'resume' (use 'commands' for the full list)";
             message_area.prepend(new_bot_message);
         }
 
-        else if (user_message.trim() === "github") {
+        else if (user_message.trim().toLowerCase() === "github") {
             window.open("https://github.com/Popsa676", "_blank");
             new_bot_message.textContent = "[opened github in new tab]";
             message_area.prepend(new_bot_message);
         }
 
-        else if (user_message.trim() === "linkedin") {
+        else if (user_message.trim().toLowerCase() === "linkedin") {
             window.open("https://www.linkedin.com/in/saaket-potluri-668488199/", "_blank");
             new_bot_message.textContent = "[opened linkedin in new tab]";
             message_area.prepend(new_bot_message);
         }
 
-        else if (user_message.trim() === "reset") {
+        else if (user_message.trim().toLowerCase() === "reset") {
             sessionStorage.setItem('conversation_history', '');
             message_area.replaceChildren();
             new_bot_message.textContent = "hey";
@@ -74,9 +74,14 @@ send_button.addEventListener("click", function() {
             message_area.prepend(new_bot_message2);
         }
 
-        else if (user_message.trim() === "resume") {
+        else if (user_message.trim().toLowerCase() === "resume") {
             window.open("https://drive.google.com/file/d/1qi_lVRh8b19T2L1YzaIiClU_nf_PmD_S/view?usp=sharing", "_blank");
             new_bot_message.textContent = "[opened resume in new tab]";
+            message_area.prepend(new_bot_message);
+        }
+
+        else if (user_message.trim().toLowerCase() === "saaketgpt") {
+            new_bot_message.textContent = "[saaket] saaketgpt is a llama 3.2 instruct model with 3 billion parameters, meaning it is a much smaller scale model than something like chatgpt. it was trained with unsloth in a google colab notebook, which is an easy method that anyone can use to train a small ai model on their own data. i first trained the llm on the finetome dataset for 1000 steps to improve the model's assistant-like qualities before doing 1000 steps on my own messages, which were scraped from discord using the browser extension discrub. overall, the model emulates my texting style reasonably for its size, which you can see for yourself by typing a message into the message bar below";
             message_area.prepend(new_bot_message);
         }
 
